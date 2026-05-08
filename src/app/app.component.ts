1ac7a05cb92ff3285b667685c02e16b9abcee04f
ATZ-code <alejandrotamayo252002@gmail.com>
2026-05-08 16:41:14 -0500

    app de colores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,57 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
 
-  display: string = '';
-  firstNumber: number | null = null;
-  operator: string = '';
+  colorSeleccionado: string = 'orange';
 
-  handleClick(value: string) {
-
-    
-    if (!isNaN(Number(value))) {
-      this.display += value;
-      return;
-    }
-
-    
-    if (value === 'C') {
-      this.display = '';
-      this.firstNumber = null;
-      this.operator = '';
-      return;
-    }
-
-    
-    if (['+', '-', '*', '/'].includes(value)) {
-      this.firstNumber = Number(this.display);
-      this.operator = value;
-      this.display = '';
-      return;
-    }
-
-    
-    if (value === '=') {
-      const secondNumber = Number(this.display);
-
-      if (this.firstNumber !== null) {
-        switch (this.operator) {
-          case '+':
-            this.display = String(this.firstNumber + secondNumber);
-            break;
-          case '-':
-            this.display = String(this.firstNumber - secondNumber);
-            break;
-          case '*':
-            this.display = String(this.firstNumber * secondNumber);
-            break;
-          case '/':
-            this.display = String(this.firstNumber / secondNumber);
-            break;
-        }
-      }
-    }
+  cambiarColor(color: string) {
+    this.colorSeleccionado = color;
   }
+
 }
